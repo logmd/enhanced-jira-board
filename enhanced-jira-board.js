432402f0e5b8bde08a86feacc3d1a8a1195b42bf
AmrikBhogal <amrik.bhogal@stepstone.co.uk>
2018-08-17 11:42:28 +0100

    remove custom fonts from styles update automatic simplify script

--- enhanced-jira-board.js
+++ enhanced-jira-board.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-var jsDevCdn = 'https://rawgit.com/logmd/enhanced-jira-board/master/src/simplify-app.js',
-jsProdCdn = 'https://cdn.rawgit.com/logmd/enhanced-jira-board/510115ee/src/simplify-app.js',
-jsCdn = jsDevCdn;
+	var jsDevCdn =
+			"https://rawgit.com/logmd/enhanced-jira-board/master/src/simplify-app.js",
+		jsProdCdn =
+			"https://cdn.rawgit.com/logmd/enhanced-jira-board/510115ee/src/simplify-app.js",
+		jsCdn = jsDevCdn;
 
-})
+	var loadJs = function() {
+		var script = document.createElement("script");
+		script.src = jsCdn;
+
+		console.log();
+		console.log(script);
+
+		document.head.appendChild(script);
+	};
+
+	loadJs();
+});
--- src/simplify-app.js
+++ src/simplify-app.js
@@ -1,74 +1,78 @@
-(function simplifyApp(){
-var cssDevCdn = 'https://rawgit.com/logmd/enhanced-jira-board/master/src/simplify-style.css',
-cssProdCdn = 'https://cdn.rawgit.com/logmd/enhanced-jira-board/510115ee/src/simplify-style.css',
-cssCdn = cssDevCdn;
-
-
-})()
-
-
+(function simplifyApp() {
+	var cssDevCdn =
+			"https://rawgit.com/logmd/enhanced-jira-board/master/src/simplify-style.css",
+		cssProdCdn =
+			"https://cdn.rawgit.com/logmd/enhanced-jira-board/510115ee/src/simplify-style.css",
+		cssCdn = cssDevCdn;
+})();
 
 function onSlimifyClick() {
+	var calculateTotalComplexity = function() {
+		var columns = $("li[data-id]").map(function() {
+			return $(this).attr("data-id");
+		});
 
-    var calculateTotalComplexity = function() {
-        var columns = $("li[data-id]").map(function() {
-            return $(this).attr("data-id");
-        });
+		columns.each(function(i, item) {
+			console.log(item);
 
-        columns.each(function(i, item) {
-            console.log(item);
+			var headerElement = $("[data-id='" + item + "'] h2");
+			console.log(headerElement[0].innerText);
 
-            var headerElement = $("[data-id='" + item + "'] h2");
-            console.log(headerElement[0].innerText);
+			var storyPoints = $(
+				"[data-column-id='" + item + "'] [title='Story Points']"
+			).map(function() {
+				return this.innerText;
+			});
+			var sum = 0;
 
-            var storyPoints = $("[data-column-id='" + item + "'] [title='Story Points']").map(function() {
-                return this.innerText
-            })
-            var sum = 0;
-
-            storyPoints.each(function(i,item) {
+			storyPoints.each(function(i, item) {
 				var parsedInt = parseInt(item);
-				if(parsedInt) sum += parsedInt;
-            });
-			
-            console.log(sum);
+				if (parsedInt) sum += parsedInt;
+			});
 
-            headerElement.after("<span class='column-total-complexity'>" + sum + "</span>");
-        })
+			console.log(sum);
 
-    }
+			headerElement.after(
+				"<span class='column-total-complexity'>" + sum + "</span>"
+			);
+		});
+	};
 
-    var enableSwaggerMode = function() {
-        var head = document.getElementsByTagName('head')[0];
-        var link = document.createElement('link');
-        link.id = 'slim-css';
-        link.rel = 'stylesheet';
-        link.type = 'text/css';
-        link.href = 'https://cdn.rawgit.com/logmd/6e3071f77eb976ea9e8556a95d2b5b81/raw/2af2cd3bf23cf98525ead7317ab13288d7226842/jira-board-cleanup.css';
-        link.media = 'all';
-        head.appendChild(link);
+	var enableSwaggerMode = function() {
+		var head = document.getElementsByTagName("head")[0];
+		var link = document.createElement("link");
+		link.id = "slim-css";
+		link.rel = "stylesheet";
+		link.type = "text/css";
+		link.href =
+			"https://cdn.rawgit.com/logmd/6e3071f77eb976ea9e8556a95d2b5b81/raw/2af2cd3bf23cf98525ead7317ab13288d7226842/jira-board-cleanup.css";
+		link.media = "all";
+		head.appendChild(link);
 
-        $.each($(".js-key-link"), function(i, item) {
-            var text = item.innerText;
-            var reg = new RegExp("([^-]+$)");
-            $(item).parent().parent().append("<span class='boring-issue-number'>"+reg.exec(text)[0]+"</span>");
-        })
+		$.each($(".js-key-link"), function(i, item) {
+			var text = item.innerText;
+			var reg = new RegExp("([^-]+$)");
+			$(item)
+				.parent()
+				.parent()
+				.append(
+					"<span class='boring-issue-number'>" + reg.exec(text)[0] + "</span>"
+				);
+		});
 
-        calculateTotalComplexity();
+		calculateTotalComplexity();
+	};
 
-    }
-
-
-
-    enableSwaggerMode();
-    console.log("slimify activated... refresh to reset state");
-
+	enableSwaggerMode();
+	console.log("slimify activated... refresh to reset state");
 }
 
 $(document).ready(function() {
-    $("#subnav-title").append('<button id="slim-button" class="aui-button" onclick="onSlimifyClick()">slimify</button>')
-    console.log("slimify has been injected");
+	$("#subnav-title").append(
+		'<button id="slim-button" class="aui-button" onclick="onSlimifyClick()">slimify</button>'
+	);
+	console.log("slimify has been injected");
 
-    var urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.has('slimify')) onSlimifyClick();
-})
+	var urlParams = new URLSearchParams(window.location.search);
+	if (urlParams.has("slimify")) onSlimifyClick();
+});
